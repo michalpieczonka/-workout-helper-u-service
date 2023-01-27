100f1b0c686ab53659d969ad0e539328d2e87444
michalpieczonka <michalpieczonka>
2023-01-27 10:30:47 +0100

    Initial repository

---
+++ src/main/java/pm/workout/helper/domain/notification/NotificationType.java
@@ -0,0 +1,5 @@
+package pm.workout.helper.domain.notification;
+
+public enum NotificationType {
+    PROFILE_UPDATE_REMINDER
+}
---
+++ src/test/java/pm/workout/helper/WorkoutHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package pm.workout.helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutHelperApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
